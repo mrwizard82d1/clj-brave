a9413b52d4253491b1f8916dc09a369c9eaaaee1
Larry Jones <mrwizard82d1@gmail.com>
2025-08-06 23:36:54 -0500

    (Finally) got REPL working for playsync.core

--- ch11/playsync/src/playsync/core.clj
+++ ch11/playsync/src/playsync/core.clj
@@ -1,7 +1,18 @@
 (ns playsync.core
-  (:gen-class))
+  (:require [clojure.core.async
+             :as async
+             :refer [>! <! >!! <!! go chan buffer
+                     close! thread alts! alts!! timeout]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(println "Starting `playsync.core`")
+
+(def echo-chan (chan))
+(go (println (<! echo-chan)))
+(>!! echo-chan "ketchup")
+
+(println "Ending `playsync.core`")
