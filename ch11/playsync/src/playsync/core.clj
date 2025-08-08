bb3b04c3b5b1a20bc11f9c3fc572675d7f8c2fee
Larry Jones <mrwizard82d1@gmail.com>
2025-08-07 19:28:21 -0500

    Passing data through **multiple** channels

--- ch11/playsync/src/playsync/core.clj
+++ ch11/playsync/src/playsync/core.clj
@@ -143,5 +143,13 @@
   (>!! in 3)                                                ;; Must have more cookies - er - hot dogs
   (<!! out))                                                ;; Hey! What about my money!
 
+;; Pass data through a sequence of channels
+(let [c1 (chan)
+      c2 (chan)
+      c3 (chan)]
+  (go (>! c2 (clojure.string/upper-case (<! c1))))          ;; From channel 1 into channel 2
+  (go (>! c3 (clojure.string/reverse (<! c2))))
+  (go (println (<! c3)))
+  (>!! c1 "redrum"))
 
 (println "Ending `playsync.core`")
