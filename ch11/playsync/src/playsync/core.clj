dd3c30185cac6db71d0cb4605443dae5b504b351
Larry Jones <mrwizard82d1@gmail.com>
2025-11-27 11:30:32 -0600

    A hot dog vending machine

--- ch11/playsync/src/playsync/core.clj
+++ ch11/playsync/src/playsync/core.clj
@@ -202,6 +202,57 @@
   (>!! in "pocket lint")
   (<!! out))
 
+;; A correct iteration of our hot dog machine
+
+(defn hot-dog-machine-v2
+  [hot-dog-count]
+  (let [in (chan)
+        out (chan)]
+    (go (loop [hc hot-dog-count]
+          (if (> hc 0)
+            (let [input (<! in)]
+              ;; Dispense a hot dog only if I received $3
+              (if (= 3 input)
+                (do (>! out "hot dog")
+                    ;; Continue operating with one less hot dog
+                    (recur (dec hc)))
+                ;; Otherwise, dispense wilted lettuce
+                (do (>! out "wilted lettuce")
+                    (recur hc))))
+            ;; When no more hot dogs, shut down everything (in detail,
+            ;; close all the channels).
+            (do (close! in)
+                (close! out)))))
+    [in out]))
+
+;; Let's give the upgraded hot dog machine a whirl
+(let [[in out] (hot-dog-machine-v2 2)]
+  ;; First try: more pocket lint
+  (>!! in "pocket lint")
+  (println (<!! out))
+
+  ;; Second try: a grudging $3
+  (>!! in 3)
+  (println (<!! out))
+
+  ;; Delish. Another!
+  (>!! in 3)
+  (println (<!! out))
+
+  ;; Third time's a charm!?
+  (>!! in 3)
+  (<!! out))
+
+;; An illustrative pipeline of channels connecting the output of one
+;; to the input of another.
+(let [c1 (chan)
+      c2 (chan)
+      c3 (chan)]
+  (go (>! c2 (clojure.string/upper-case (<! c1))))
+  (go (>! c3 (clojure.string/reverse (<! c2))))
+  (go (println (<! c3)))
+  (>!! c1 "redrum"))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
