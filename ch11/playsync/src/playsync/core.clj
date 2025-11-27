3df8d28d833ae1158d945fd70efab5348d29af05
Larry Jones <mrwizard82d1@gmail.com>
2025-11-27 12:19:38 -0600

    Beginning demonstrations of `alt!!`

--- ch11/playsync/src/playsync/core.clj
+++ ch11/playsync/src/playsync/core.clj
@@ -253,6 +253,32 @@
   (go (println (<! c3)))
   (>!! c1 "redrum"))
 
+;; ## alts!!
+
+;; > The `core.async` function `alts!!` lets you use the result of
+;; > the **first** successful channel operation among a collection
+;; > of operations. We did something similar to this with delays
+;; > and futures in "Delays" on page 198. In that example, we
+;; > uploaded a set of headshots to a headshot sharing site and
+;; > notified the headshot owner when the first photo was uploaded.
+;; > Here's how you'd do the same with `alt!!`:
+
+#_(defn upload
+    [headshot c]
+    (go (Thread/sleep (rand 100))
+        (>! c headshot)))
+
+(rand 100)
+
+#_(let [c1 (chan)
+        c2 (chan)
+        c3 (chan)]
+    (upload "serious.jpg" c1)
+    (upload "fun.jpg" c2)
+    (upload "sassy.jpg" c3)
+    (let [[headshot channel] (alts!! [c1 c2 c3])]
+      (println "Sending headshot notification for" headshot)))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
