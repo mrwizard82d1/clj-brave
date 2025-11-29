3487249c36796ba3c0cf6d989c273e865a31f878
Larry Jones <mrwizard82d1@gmail.com>
2025-11-29 14:20:24 -0600

    Promises

--- ch09/concurrent-parallel/src/concurrent_parallel/core.clj
+++ ch09/concurrent-parallel/src/concurrent_parallel/core.clj
@@ -120,3 +120,111 @@
 ;; On the other hand, no thread can ever use the delay to send an email
 ;; again. This constraint might be too drastic for most situations, but
 ;; it works perfectly in cases like this.
+
+;; ### Promises
+
+;; _Promises_ express the idea that I expect a result **without yet**
+;;
+;; - Defining the task that should produce the result
+;; - Determining when that task should run
+;;
+;; One creates a promisg using the `promise` function. One delivers a
+;; result to a promise using `deliver`. Finally, you obtain the result
+;; by dereferencing.
+
+(def my-promise (promise))
+
+(deliver my-promise (+ 1 2))
+
+@my-promise
+
+;; Remember, if one dereferences a `promise` **before** delivering a
+;; value, the code will **block** until a result is available.
+;;
+;; You can only deliver a result to a promise **once**.
+
+;; One use for a promise is to find the first (in time) satisfactory
+;; element in a collection of data generated at different times.
+
+;; Premium yak butter
+
+(def yak-butter-international
+  {:store "Yak Butter International"
+   :price 90
+   :smoothness 90})
+
+(def butter-than-nothing
+  {:store "Butter Than Nothing"
+   :price 150
+   :smoothness 83})
+
+;; Here is the butter that meets our requirements.
+(def baby-got-yak
+  {:store "Baby Got Yak"
+   :price 94
+   :smoothness 99})
+
+(defn mock-api-call
+  [result]
+  ;; Simulate a long-running function call
+  (Thread/sleep 1000)
+  result)
+
+(defn satisfactory?
+  "If the butter meets our criterian, return the butter, else return false"
+  [butter]
+  (and (<= (:price butter) 100)
+       (>= (:smoothness butter) 97)
+       butter))
+
+;; Making a call with `some` demonstrates how to apply the i
+;; ``satisfactory?` predicate and return the first truthy result. It
+;; should take about second.`
+
+(time (some (comp satisfactory? mock-api-call)
+            [yak-butter-international butter-than-nothing baby-got-yak]))
+
+;; One can use a promise and futures to check each option on a different
+;; thread. With multiple cores, this implention should reduce the time it
+;; takes to about one second.
+
+(time
+ ;; We begin by creating a `promise` to deliver a value
+ (let [butter-promise (promise)]
+   (doseq [butter [yak-butter-international
+                   butter-than-nothing
+                   baby-got-yak]]
+     ;; Create three futures to calculate the promise ruselt
+     (future (if-let [satisfactory-butter (satisfactory? (mock-api-call butter))]
+               (deliver butter-promise satisfactory-butter))))
+   ;; Dereference the promise causing the program to wait unit a
+   ;; satisfactory butter is found.
+   (println "And the winner is:" @butter-promise)))
+
+;; This code demonstrates a way to protect yourself from the "reference cell
+;; Concurrency Goblin" because promises can only be written to once (first
+;; writer wins). Consequently, these constructs prevent the kind of
+;; inconsistent state that arises from nondeterministic reads and writes.
+
+;; You might be wondering what happens if **no** yak butter option is
+;; satisfactory. This circumstance would result in the dereference
+;; blocking the current thread **forever**. To avoid that, you can
+;; include a timeout.
+
+;; The following expression waits for 100 ms to resolve the promise
+;; Since no result will be delivered, this code returns the text,
+;; "timed out".
+(let [p (promise)]
+  (deref p 100 "timed out"))
+
+;; Finally, one can use promises to register callbacks achieving the
+;; same functionality available in JavaScript. Here's how:
+
+(let [ferengi-wisdom-promise (promise)]
+  ;; The future begins executing immediately; however, dereferencing
+  ;; the primes **blockes** until it is available.
+  (future (println "Here's some Ferengi wisdom:" @ferengi-wisdom-promise))
+  ;; Simualate a delay by sleeping
+  (Thread/sleep 100)
+  ;; Deliver the promise
+  (deliver ferengi-wisdom-promise "Whisper your way to success."))
